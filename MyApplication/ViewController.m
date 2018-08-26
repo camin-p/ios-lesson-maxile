0caf75f574dea84fcf350b87dd3d2efb504a608e
Camin <admin@maxile.net>
2018-08-26 16:33:33 +0700

    Initial Commit

---
+++ MyApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyApplication
+//
+//  Created by Maxile on 26/8/2561 BE.
+//  Copyright © 2561 Maxile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MyApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyApplication
+//
+//  Created by Maxile on 26/8/2561 BE.
+//  Copyright © 2561 Maxile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
