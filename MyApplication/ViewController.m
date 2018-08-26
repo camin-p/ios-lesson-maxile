12b40c43a1c88486410f4de6575756b85a6d821f
Camin <admin@maxile.net>
2018-08-27 06:46:27 +0700

    complete view

--- MyApplication/ViewController.m
+++ MyApplication/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import <QuartzCore/QuartzCore.h>
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *mainBtn;
 
 @end
 
@@ -17,6 +18,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [_mainBtn layer].cornerRadius=10;
+    [_mainBtn setClipsToBounds:NO];
+    [[_mainBtn layer] setShadowColor:[UIColor blackColor].CGColor];
+    [[_mainBtn layer] setShadowOpacity:0.24];
+    [[_mainBtn layer]setShadowRadius:2];
+    [[_mainBtn layer] setShadowOffset:CGSizeMake(0, 2)];
 }
 
 
